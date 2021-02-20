7d642e257c3379a93101eac51c2bfa4bd0eac133
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-20 11:12:33 -0500

    Initial Commit

---
+++ ForeignExchangeApp/ForeignExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeignExchangeAppApp.swift
+//  ForeignExchangeApp
+//
+//  Created by Evan Armstrong on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct ForeignExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
