9a5f9f9c6305c7da405f64a5756a0361fe308155
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-21 22:43:07 -0500

    added all the currencies to the project.

---
+++ ForeignExchangeApp/Preview Content/Currency rates.swift
@@ -0,0 +1,45 @@
+//
+//  Currency rates.swift
+//  ForeignExchangeApp
+//
+//  Created by Evan Armstrong on 2021-02-21.
+//
+
+import Foundation
+
+struct rates: Decodable {
+    let CAD: Int
+    let HKD: Int
+    let ISK: Int
+    let PHP: Int
+    let DKK: Int
+    let HUF: Int
+    let CZK: Int
+    let AUD: Int
+    let RON: Int
+    let SEK: Int
+    let IDR: Int
+    let INR: Int
+    let BRL: Int
+    let RUB: Int
+    let HRK: Int
+    let JPY: Int
+    let THB: Int
+    let CHF: Int
+    let SGD: Int
+    let PLN: Int
+    let BGN: Int
+    let TRY: Int
+    let CNY: Int
+    let NOK: Int
+    let NZD: Int
+    let ZAR: Int
+    let USD: Int
+    let MXN: Int
+    let ILS: Int
+    let GBP: Int
+    let KRW: Int
+    let MYR: Int
+    let base: String
+    
+}
